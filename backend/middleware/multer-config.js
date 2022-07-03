03a0994dd13fc063516643bfab66796497668495
GabrielleDFT <96197835+GabrielleDFT@users.noreply.github.com>
2022-07-03 05:15:11 +0200

    Update multer-config.js

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,7 +1,9 @@
-              //---- FILE MANAGEMENT ----
-const multer = require("multer");
+//----------------------------------------------- FILE MANAGEMENT ------------------------------------------------
 
-//--Dictionary MIME types to define the images format-- 
+// On importe multer qui est un package qui permet de gérer les fichiers entrants dans les requêtes HTTP
+const multer = require('multer');
+
+//--Dictionary MIME types to define the images format-- Create an object
 const MIME_TYPES = {
   "image/jpg": "jpg",
   "image/jpeg": "jpg",
@@ -12,16 +14,25 @@ const MIME_TYPES = {
 const storage = multer.diskStorage({
     //--File Destination to save Images file-- 
     destination: (req, file, callback) => { 
-    callback(null, "images");
+    // On passe le dossier images qu'on a créé dans le backend
+    callback(null, 'images');
   },
     //--File Name-- 
-  filename: (req, file, callback) => { 
+   filename: (req, file, callback) => { 
     //--New name - Replacement of " " by "_" in file name--
     const name = file.originalname.split(" ").join("_"); 
     //--Setting up file extension--
     const extension = MIME_TYPES[file.mimetype]; 
-    callback(null, name + Date.now() + "." + extension); //Assemblage du nom 
+
+    //--Call to callback +  null when no error / Create filename + add timestamp, dot & extension file
+    callback(null, name + Date.now() + "." + extension); 
   },
 });
 
+//--Export module, pass storage object (single method for unique file & specify image)
 module.exports = multer({ storage: storage }).single('image'); 
+
+
+
+
+
