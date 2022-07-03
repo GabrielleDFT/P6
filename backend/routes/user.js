53dc9c08d00810b9c1eba83d6d8de5ab3da5a5b4
GabrielleDFT <96197835+GabrielleDFT@users.noreply.github.com>
2022-07-03 04:31:48 +0200

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,12 +1,21 @@
-                 //---- USER ROAD ----
+//---------------------------- USER ROUTES --------------------------------
+
+//--Add plugin to use Express router--
 const express = require("express");
+
+//--Call to Router with express method-- 
 const router = express.Router();
+
+//--Associate functions to differents routes, - Import Controller--
 const userCtrl = require("../controllers/user");
 
-//--Register--
+//--Register - Encrypts User password, add User to Database--
 router.post("/signup", userCtrl.signup);
 
-//--Log in--
+//--Log in - Vérify infos from User
 router.post("/login", userCtrl.login);
 
 module.exports = router;
+
+
+
