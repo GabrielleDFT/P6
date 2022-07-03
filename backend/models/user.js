dd484a7b3e662401a9e91dcb74006a261ba06191
GabrielleDFT <96197835+GabrielleDFT@users.noreply.github.com>
2022-07-03 04:12:32 +0200

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,9 @@
-                //---- USER MODEL ----
+//----------------------------- USER MODEL --------------------------------
+
+//--Creating User Model with Mongoose - Import Mongoose--
 const mongoose = require("mongoose");
+
+//--mongoose-unique-validator passed as plugin = 2 Users cannot share the same Email address--
 const uniqueValidator = require("mongoose-unique-validator");
 
 //--Database model to Signup (Register a New User)--
@@ -11,4 +15,13 @@ const userSchema = mongoose.Schema({
 //--Verification for Single Email Address--
 userSchema.plugin(uniqueValidator);
 
+//--Export model schéma : the model called User & pass the data schema
 module.exports = mongoose.model("user", userSchema);
+
+
+
+
+
+
+
+
