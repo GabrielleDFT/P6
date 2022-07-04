2ec5d012beb3c7ad1df1cb1dbe9649700f22a93c
GabrielleDFT <96197835+GabrielleDFT@users.noreply.github.com>
2022-07-05 01:32:32 +0200

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,8 +1,6 @@
 //----------------------------------------------------- USER VERIFICATION SAFE ROADS -------------------------------------------------------
 
-//Retrieve jsonwebtoken package
 const jwt = require("jsonwebtoken");
-
 require("dotenv").config();
 
 //--Middleware applied to all Routes to secure them--
@@ -10,16 +8,12 @@ module.exports = (req, res, next) => {
     try {
       //--Generated Key Recovery - Retrieving Token in authorization request Header, retrieve 2eme element from array (car split)--
       const token = req.headers.authorization.split(" ")[1];
-                
       //--Key Decoding - Checking decoded Token with initiated secret key (Cf Controller user)--
       const decodedToken = jwt.verify(token, process.env.TOKEN);
-                
       //--ID Recovery in TOKEN - Checking userId sent with request matches to userId encoded in Token--
       const userId = decodedToken.userId;
-                
       //--ID retrieval in request--
-      req.auth = { userId };
-                
+    //  req.auth = { userId };
      //--IDs Comparison--
       if (req.body.userId && req.body.userId !== userId) {
         throw "Invalid user ID";//--If Token don't match with userId : error--
@@ -32,6 +26,5 @@ module.exports = (req, res, next) => {
       });
     }
 };
-
 
 
